90389ec8ddbdeb2656a35f5d3ee8552687a1c907
Ari Pringle <apringle@gmail.com>
2018-05-30 13:24:46 -0600

    Initial commit (#1)

---
+++ src/RoboFile.php
@@ -0,0 +1,322 @@
+<?php declare(strict_types=1);
+namespace DiabloMedia\Robo;
+
+use DateTime;
+use Exception;
+use Robo\Result;
+use Symfony\Component\Console\Input\InputOption;
+
+abstract class RoboFile extends \Robo\Tasks
+{
+    use Task\MysqldumpPhp\Tasks;
+    use Task\Mysql\Tasks;
+
+    /**
+     * @var string
+     */
+    protected $hostname;
+
+    /**
+     * @var string
+     */
+    protected $database;
+
+    /**
+     * @var string
+     */
+    protected $password;
+
+    /**
+     * @var string
+     */
+    protected $dsn;
+
+    /**
+     * @var string
+     */
+    protected $username = 'root';
+
+    /**
+     * @var string
+     */
+    protected $dumpDir;
+
+    /**
+     * @var string
+     */
+    protected $dumpSchemaFile = '00-Schema.sql';
+
+    /**
+     * @var string
+     */
+    protected $dumpFullFile   = '05-FullTables.sql';
+
+    /**
+     * @var string
+     */
+    protected $dumpGrantsFile = '9999-Grants.sql';
+
+    public function __construct()
+    {
+        $required = [
+            'hostname', 'database', 'dumpDir'
+        ];
+
+        foreach ($required as $property) {
+            if ($this->$property === null) {
+                throw new Exception('Property ' . $property . ' must have a value declared');
+            }
+        }
+
+        if ($this->dsn === null) {
+            $this->dsn = 'mysql:host=' . $this->hostname . ';dbname=' . $this->database . ';chartset=utf8';
+        }
+    }
+
+    /**
+     * @return string[]
+     */
+    abstract protected function getGrantUsers();
+
+    /**
+     * @return string[]
+     */
+    abstract protected function getDefaultDataFilters();
+
+    /**
+     * @return string[]
+     */
+    abstract protected function getDefaultDataExcludes();
+
+    /**
+     * @return string|false
+     */
+    protected function getDataFilterForTable($table)
+    {
+        $filters = $this->getDefaultDataFilters();
+        return $filters[$table] ?? false;
+    }
+
+    /**
+     * @return string
+     */
+    protected function getPassword(string $password = null)
+    {
+        if ($password) {
+            $this->password = $password;
+        }
+
+        if ($this->password === null) {
+            $this->password = $this->askHidden('Password for ' . $this->username . '@' . $this->hostname);
+        }
+
+        return $this->password;
+    }
+
+    public function dbDumpSchema(
+        array $opts = [
+            'file'           => InputOption::VALUE_REQUIRED,
+            'password'       => InputOption::VALUE_REQUIRED,
+            'include-tables' => InputOption::VALUE_REQUIRED,
+            'exclude-tables' => InputOption::VALUE_REQUIRED
+        ]
+    ) : Result {
+        $file = $opts['file'] ?? $this->dumpDir . DIRECTORY_SEPARATOR . $this->dumpSchemaFile;
+
+        $includeTables = [];
+        $excludeTables = [];
+
+        $pass = $this->getPassword($opts['password']);
+
+        if (!empty($opts['include-tables'])) {
+            $includeTables = explode(',', $opts['include-tables']);
+        }
+
+        if (!empty($opts['exclude-tables'])) {
+            $excludeTables = explode(',', $opts['exclude-tables']);
+        }
+
+        $this->say('Dumping prod schema (no data)');
+        $this->say(sprintf('  Including: %s', empty($includeTables) ? 'All' : implode(', ', $includeTables)));
+        $this->say(sprintf('  Excluding: %s', empty($excludeTables) ? 'None' : implode(', ', $excludeTables)));
+
+        return $this->taskDumpSchema($this->dsn, $this->username, $pass)
+            ->getCollectionBuilderCurrentTask()
+            ->withDumpSettings([
+                'include-tables' => $includeTables,
+                'exclude-tables' => $excludeTables,
+                'lock-tables'    => false
+            ])
+            ->toFile($file)
+            ->run();
+    }
+
+    public function dbDumpDataFull(
+        array $opts = [
+            'file'                    => InputOption::VALUE_REQUIRED,
+            'password'                => InputOption::VALUE_REQUIRED,
+            'include-tables'          => InputOption::VALUE_REQUIRED,
+            'exclude-tables'          => InputOption::VALUE_REQUIRED,
+            'ignore-default-excludes' => false
+        ]
+    ) : Result {
+        $file = $opts['file'] ?? $this->dumpDir . DIRECTORY_SEPARATOR . $this->dumpFullFile;
+
+        $pass = $this->getPassword($opts['password']);
+
+        $includeTables = [];
+        $excludeTables = [];
+
+        if (!empty($opts['include-tables'])) {
+            $includeTables = explode(',', $opts['include-tables']);
+        }
+
+        if ($opts['ignore-default-excludes'] === false) {
+            // By default, exclude all tables in the exclude & partial lists
+            $excludeTables = array_merge($this->getDefaultDataExcludes(), array_keys($this->getDefaultDataFilters()));
+        }
+
+        $this->say('Dumping full tables');
+        $this->say(sprintf('  Including: %s', empty($includeTables) ? 'All' : implode(', ', $includeTables)));
+        $this->say(sprintf('  Excluding: %s', empty($excludeTables) ? 'None' : implode(', ', $excludeTables)));
+
+        return $this->taskDumpData($this->dsn, $this->username, $pass)
+            ->getCollectionBuilderCurrentTask()
+            ->withDumpSettings([
+                'include-tables' => $includeTables,
+                'exclude-tables' => $excludeTables,
+                'lock-tables'    => false
+            ])
+            ->toFile($file)
+            ->run();
+    }
+
+
+    public function dbDumpDataPartial(
+        string $startDate,
+        string $endDate,
+        array $opts = [
+            'dir'               => InputOption::VALUE_REQUIRED,
+            'append'            => false,
+            'include-tables'    => InputOption::VALUE_REQUIRED,
+            'exclude-tables'    => InputOption::VALUE_REQUIRED,
+            'starting-sequence' => '10',
+            'password'          => InputOption::VALUE_REQUIRED,
+        ]
+    ) : Result {
+        $dir = $opts['dir'] ?? $this->dumpDir;
+
+        $pass = $this->getPassword($opts['password']);
+
+        $startDate = DateTime::createFromFormat('Y-m-d', $startDate);
+        if ($startDate === false) {
+            throw new Exception('Start Date provided is not in the correct format');
+        }
+
+        $endDate = DateTime::createFromFormat('Y-m-d', $endDate);
+        if ($endDate === false) {
+            throw new Exception('End Date provided is not in the correct format');
+        }
+
+        if (!empty($opts['include-tables'])) {
+            $includeTables = explode(',', $opts['include-tables']);
+        } else {
+            $includeTables = array_keys($this->getDefaultDataFilters());
+        }
+
+        if (!empty($opts['exclude-tables'])) {
+            $includeTables = array_diff($includeTables, explode(',', $opts['exclude-tables']));
+        }
+
+        $filters = [];
+        foreach ($includeTables as $table) {
+            $where = $this->getDataFilterForTable($table);
+            if ($where === false) {
+                throw new Exception('Table does not have partial filtering enabled: ' . $table);
+            }
+
+            $where           = str_replace(':startDate', $startDate->format("'Y-m-d'"), $where);
+            $where           = str_replace(':endDate', $endDate->format("'Y-m-d'"), $where);
+            $filters[$table] = $where;
+        }
+
+        $this->say('Dumping partial data for tables:');
+        $this->say('  ' . implode(', ', $includeTables));
+
+        $task = $this->taskDumpDataPartial($this->dsn, $this->username, $pass)
+            ->getCollectionBuilderCurrentTask()
+            ->toDir($dir)
+            ->withDumpSettings(['lock-tables' => false])
+            ->withFilters($filters)
+            ->startingFileSequence($opts['starting-sequence']);
+
+        if ($opts['append'] === true) {
+            $task->append();
+        }
+
+        return $task->run();
+    }
+
+    public function dbDumpGrants(
+        array $opts = [
+            'file'       => InputOption::VALUE_REQUIRED,
+            'password'   => InputOption::VALUE_REQUIRED,
+            'appendFile' => false
+        ]
+    ) : Result {
+        $file = $opts['file'] ?? $this->dumpDir . DIRECTORY_SEPARATOR . $this->dumpGrantsFile;
+
+        $pass = $this->getPassword($opts['password']);
+
+        $users      = $this->getGrantUsers();
+        $collection = $this->collectionBuilder();
+
+        $appendFile = $opts['appendFile'];
+        foreach ($users as $user) {
+            $this->say(sprintf('Dumping grants for %s@%s', $user['user'], $user['host']));
+            $task = $collection->taskDumpGrants($this->dsn, $this->username, $pass)
+                ->sourceUser($user['user'], $user['host'])
+                ->append($appendFile)
+                ->toFile($file);
+
+            if (isset($user['pass'])) {
+                $task->destinationPassword($user['pass']);
+            }
+
+            // Any additional dumps should always append to the file
+            $appendFile = true;
+        }
+
+        return $collection->run();
+    }
+
+    public function dbInitializeDev(
+        string $startDate,
+        string $endDate
+    ): void {
+        $commands = [
+            'db:dump-schema',
+            'db:dump-data-full',
+            'db:dump-data-partial',
+            'db:dump-grants'
+        ];
+
+        $pass = $this->getPassword();
+
+        foreach ($commands as $command) {
+            if ($command == 'db:dump-data-partial') {
+                $arguments = ['vendor/bin/robo', $command, $startDate, $endDate, '--password', $pass];
+            } else {
+                $arguments = ['vendor/bin/robo', $command, '--password', $pass];
+            }
+            $statusCode = \Robo\Robo::run(
+                $arguments,
+                self::class
+            );
+
+            if ($statusCode !== 0) {
+                throw new Exception('Command ' . $command . ' failed');
+            }
+        }
+    }
+}
---
+++ src/Task/Mysql/DumpGrants.php
@@ -0,0 +1,172 @@
+<?php declare(strict_types=1);
+namespace DiabloMedia\Robo\Task\Mysql;
+
+use PDO;
+use Robo\Result;
+use Robo\Task\BaseTask;
+use Robo\Common\BuilderAwareTrait;
+use Robo\Contract\BuilderAwareInterface;
+
+class DumpGrants extends BaseTask implements BuilderAwareInterface
+{
+    use BuilderAwareTrait;
+
+    /**
+     * @var string
+     */
+    protected $dsn;
+
+    /**
+     * @var string
+     */
+    protected $user;
+
+    /**
+     * @var string
+     */
+    protected $pass;
+
+    /**
+     * @var string
+     */
+    protected $file;
+
+    /**
+     * @var string
+     */
+    protected $sourceUser;
+
+    /**
+     * @var string
+     */
+    protected $sourceHost;
+
+    /**
+     * @var string
+     */
+    protected $destinationUser;
+
+    /**
+     * @var string
+     */
+    protected $destinationHost;
+
+    /**
+     * @var string
+     */
+    protected $destinationPassword;
+
+    /**
+     * @var bool
+     */
+    protected $append;
+
+    public function __construct(string $dsn, string $user, string $pass)
+    {
+        $this->dsn  = $dsn;
+        $this->user = $user;
+        $this->pass = $pass;
+    }
+
+    public function sourceUser(string $grantUser, string $grantHost) : DumpGrants
+    {
+        $this->sourceUser = $grantUser;
+        $this->sourceHost = $grantHost;
+
+        return $this;
+    }
+
+    public function destinationUser(string $grantUser, string $grantHost) : DumpGrants
+    {
+        $this->destinationUser = $grantUser;
+        $this->destinationHost = $grantHost;
+
+        return $this;
+    }
+
+    public function destinationPassword(string $password) : DumpGrants
+    {
+        $this->destinationPassword = $password;
+
+        return $this;
+    }
+
+    public function toFile(string $file) : DumpGrants
+    {
+        $this->file = $file;
+
+        return $this;
+    }
+
+    public function append(bool $append = true) : DumpGrants
+    {
+        $this->append = $append;
+
+        return $this;
+    }
+
+    protected function replaceUser(string $line) : string
+    {
+        if (!empty($this->destinationUser)) {
+            $from = sprintf("/'%s'@'%s'/", $this->sourceUser, $this->sourceHost);
+            $to   = sprintf("'%s'@'%s'", $this->destinationUser, $this->destinationHost);
+            return preg_replace($from, $to, $line);
+        }
+
+        return $line;
+    }
+
+    protected function replacePassword(string $line) : string
+    {
+        if (!empty($this->destinationPassword)) {
+            return preg_replace('/PASSWORD <secret>/', "'" . $this->destinationPassword . "'", $line);
+        }
+
+        // If no password specified, remove the IDENTIFIED BY section completely
+        return preg_replace('/ IDENTIFIED BY PASSWORD <secret>/', '', $line);
+    }
+
+    protected function prepareLineForOutput(string $line) : string
+    {
+        $line = $this->replaceUser($line);
+        $line = $this->replacePassword($line);
+        return $line . ';';
+    }
+
+    public function run() : Result
+    {
+        if (!extension_loaded('PDO')) {
+            return Result::errorMissingExtension($this, 'PDO', 'database connectivity');
+        }
+
+        if (empty($this->sourceUser) || empty($this->sourceHost)) {
+            return Result::error($this, 'sourceUser must be called');
+        }
+
+        if (empty($this->file)) {
+            return Result::error($this, 'Filename must be specified with toFile method');
+        }
+
+        $pdoOpt = [
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_NUM
+        ];
+        $pdo   = new PDO($this->dsn, $this->user, $this->pass, $pdoOpt);
+        $query = "SHOW GRANTS FOR ?@?";
+        $stmt  = $pdo->prepare($query);
+        $stmt->execute([$this->sourceUser, $this->sourceHost]);
+
+        if ($stmt->rowCount() === 0) {
+            return Result::error($this, 'No GRANTs found for ' . $this->sourceUser . '@' . $this->sourceHost);
+        }
+
+        $collection = $this->collectionBuilder();
+        $task       = $collection->taskWriteToFile($this->file)
+            ->append($this->append);
+
+        while ($row = $stmt->fetch()) {
+            $task->line($this->prepareLineForOutput($row[0]));
+        }
+
+        return $collection->run();
+    }
+}
